90e0c388603d99cdbaf51f242abc4e0996da7842
Lazar <lazov_lazar@yahoo.com>
2019-12-25 15:19:23 +0100

    install lodash and validator setup

--- controllers/index.js
+++ controllers/index.js
@@ -1,5 +1,5 @@
 const models = require("../models");
 
 exports.index = function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Online Shop', user: req.user });
 };
